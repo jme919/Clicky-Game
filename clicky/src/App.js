76bd534e3966c965c254e3b7e6c8ae832e20098d
Unknown <jme919@gmail.com>
2018-06-13 20:22:53 -0400

    begin to create files for components

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+
+const App = () =>
+<Router> 
+  <div>
+    <Navbar />
+    <Wrapper>
+    </Wrapper>  
+
+  </div>
+</Router>  
 
 export default App;
---
+++ clicky/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+const Navbar = props => 
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+            <a>Memory Star Trek</a>
+            <a> Click an image to begin!</a>
+            <a className="button is-large">Score: 0...</a>
+            <a className="button is-large">Top Score: 0</a>
+
+        </div>
+    </nav>     
+
+
+
+
+
+export default Navbar;

6deda65f70da6fefc249544210dedf2e9e0ee388
Unknown <jme919@gmail.com>
2018-06-16 13:46:44 -0400

    fix header margin and color

--- clicky/src/components/Navbar/Navbar.js
+++ clicky/src/components/Navbar/Navbar.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = props =>
-    <header className="container bg-info">
+    <header id="header" className="container-fluid bg-info">
         <div className="row">
-            <div className="col-sm-4 h3 mmc">Moving Memory Click</div>
-            <div className="col-sm-4 h3 cai">Click an Image</div>
-            <div className="col-sm-4">
+            <div className="col-sm-4 col-m-4 h3 mmc">Star Trek Memory Game</div>
+            <div className="col-sm-4 col-m-4 h3 cai">Click an Image</div>
+            <div className="col-sm-4 col-m-4" >
                 <div className="container">
                     <div className="row h3">Score:
             <div className="col-sm-6" id="currentScore">{props.score}</div>
